ce2e17dbf74ee73bd8b444ae1a9310fe72fd3f94
stanko-dev <stanislavkovalmain@gmail.com>
2025-05-08 20:39:35 +0300

    implemented command-line parameters and basic Express server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Server will be implemented here");
+const { program } = require('commander');
+const express = require('express');
+
+program
+  .requiredOption('-h, --host <host>', 'Server host')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <cache>', 'Cache directory')
+  .parse(process.argv);
+
+const { host, port, cache } = program.opts();
+
+const app = express();
+
+const startServer = () => {
+  app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+  });
+};
+
+startServer();
